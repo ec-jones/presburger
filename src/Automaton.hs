cc4c4863f98d043b790fc7426ef5cafe6aa631b8
Eddie Jones <eddiejones2108@gmail.com>
2020-10-11 11:25:20 +0100

    functional delta

--- src/Automaton.hs
+++ src/Automaton.hs
@@ -10,35 +10,33 @@ data State
   | Set [State]
   deriving (Eq)
 
+fromBool :: Bool -> State
+fromBool False = Off
+fromBool True = On
+
 -- A (non-)deterministic finite automata over bit vectors
 data Automaton = Automaton
   { states :: [State],
     dim :: Int,
-    delta :: State -> [([Bool], State)],
+    delta :: State -> [Bool] -> [State],
     start :: State,
     final :: [State]
   }
 
 -- x + y = z
-equation :: Automaton
-equation =
+equation :: Int -> Int -> Int -> Automaton
+equation x y z =
   Automaton
     { states = [Off, On], -- to carry or not
-      dim = 3,
-      delta = \q ->
+      dim = maximum [x + 1, y + 1, z + 1],
+      delta = \q ns ->
         case q of
-          Off ->
-            [ ([False, False, True], Off),
-              ([False, True, False], On),
-              ([True, False, False], On),
-              ([True, True, True], On)
-            ]
-          On ->
-            [ ([False, False, False], Off),
-              ([False, True, True], Off),
-              ([True, False, True], Off),
-              ([True, True, False], Off)
-            ]
+          Off
+            | ns !! z == (ns !! x == ns !! y) -> [fromBool (ns !! x || ns !! y)]
+            | otherwise -> []
+          On
+            | ns !! z == (ns !! x /= ns !! y) -> [fromBool (ns !! x && ns !! y)]
+            | otherwise -> []
           _ -> [],
       start = Off,
       final = [Off]
@@ -46,26 +44,35 @@ equation =
 
 -- An automaton with n set to zero or one
 zero :: Int -> Automaton
-zero n =
+zero x =
   Automaton
     { states = [Off],
-      dim = n,
-      delta = const [(False : ns, Off) | ns <- replicateM (n - 1) [False, True]],
+      dim = x + 1,
+      delta = \q ns ->
+        case q of
+          Off
+            | ns !! x -> [Off]
+            | otherwise -> []
+          _ -> [],
       start = Off,
       final = [Off]
     }
 
 one :: Int -> Automaton
-one n =
+one x =
   Automaton
     { states = [Off, On], -- first digit
-      dim = n,
-      delta = \i ->
-        case i of
-          On -> [(True : ns, On) | ns <- replicateM (n - 1) [False, True]]
-          Off -> [(False : ns, On) | ns <- replicateM (n - 1) [False, True]]
+      dim = x + 1,
+      delta = \q ns ->
+        case q of
+          On
+            | ns !! x -> [Off]
+            | otherwise -> []
+          Off
+            | ns !! x -> []
+            | otherwise -> [Off]
           _ -> [],
-      start = Off,
+      start = On,
       final = [Off, On]
     }
 
@@ -73,11 +80,8 @@ one n =
 isDeterministic :: Automaton -> Bool
 isDeterministic a = all (isFunc . delta a) (states a)
   where
-    isFunc [] = True
-    isFunc ((x, y) : xys) =
-      case lookup x xys of
-        Nothing -> isFunc xys
-        Just z -> y == z && isFunc xys
+    isFunc :: ([Bool] -> [State]) -> Bool
+    isFunc f = all (\ns -> length (f ns) == 1) (replicateM (dim a) [False, True])
 
 -- The powet set construction
 determinise :: Automaton -> Automaton
@@ -85,13 +89,10 @@ determinise a =
   Automaton
     { states = fmap Set (subsequences (states a)),
       dim = dim a,
-      delta = \qs ->
-        case qs of
-          Set qs' ->
-            [ (n, Set [q' | (m, q') <- delta a q, m == n])
-              | q <- qs',
-                (n, _) <- delta a q
-            ]
+      delta = \q ns ->
+        case q of
+          Set qs ->
+            [Set (qs >>= \q' -> delta a q' ns)]
           _ -> [],
       start = Set [start a],
       final =
@@ -111,41 +112,37 @@ empty a = snd $ search (start a) []
       | q `elem` qs = (qs, False) -- q has already been visited
       | otherwise =
         foldr
-          ( \(_, q') (qs', found) ->
+          ( \q' (qs', found) ->
               if found
                 then (qs', True)
                 else search q' qs'
           )
           (q : qs, False)
-          (delta a q)
+          (replicateM (dim a) [False, True] >>= delta a q)
 
 intersection :: Automaton -> Automaton -> Automaton
-intersection a b
-  | dim a /= dim b = error "The automata's dimensions do not match!"
 intersection a b =
-  Automaton
-    { states =
-        [ Pair qa qb
-          | qa <- states a,
-            qb <- states b
-        ],
-      dim = dim a,
-      delta = \q ->
-        case q of
-          Pair qa qb ->
-            [ (n, Pair qa' qb')
-              | (n, qa') <- delta a qa,
-                (m, qb') <- delta b qb,
-                n == m
+  let a' = antiproject (dim a) a
+      b' = antiproject (dim b) b
+   in Automaton
+        { states =
+            [ Pair qa qb
+              | qa <- states a',
+                qb <- states b'
+            ],
+          dim = dim a',
+          delta = \q ns ->
+            case q of
+              Pair qa qb ->
+                [Pair qa' qb' | qa' <- delta a' qa ns, qb' <- delta b' qb ns]
+              _ -> [],
+          start = Pair (start a') (start b'),
+          final =
+            [ Pair qa qb
+              | qa <- final a',
+                qb <- final b'
             ]
-          _ -> [],
-      start = Pair (start a) (start b),
-      final =
-        [ Pair qa qb
-          | qa <- final a,
-            qb <- final b
-        ]
-    }
+        }
 
 union :: Automaton -> Automaton -> Automaton
 union a b =
@@ -175,7 +172,7 @@ project a =
   Automaton
     { states = states a,
       dim = dim a - 1,
-      delta = \q -> [(tail n, q') | (n, q') <- delta a q],
+      delta = \q ns -> [ns ++ [False], ns ++ [True]] >>= delta a q,
       start = start a,
       final = final a
     }
@@ -186,12 +183,7 @@ antiproject n a =
   Automaton
     { states = states a,
       dim = max (dim a) n,
-      delta = \q -> [(m, q') | (n, q') <- delta a q, m <- extend n],
+      delta = \q ns -> delta a q (take (dim a) ns),
       start = start a,
       final = final a
     }
-  where
-    extend :: [Bool] -> [[Bool]]
-    extend xs
-      | length xs >= n = [xs]
-      | otherwise = [e ++ xs | e <- replicateM (length xs - n) [False, True]]
