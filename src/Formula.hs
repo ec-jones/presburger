890581f78bc6279d27dfba62af16a50dffe8361d
Eddie Jones <eddiejones2108@gmail.com>
2020-10-12 10:16:35 +0100

    standardise after normalising atoms

--- src/Formula.hs
+++ src/Formula.hs
@@ -2,102 +2,74 @@
 
 module Formula where
 
-import Control.Monad.State (MonadState (get, put), State, evalState)
-
+import Control.Monad.State
 data Term a
   = Var a
   | Zero
   | One
   | Add (Term a) (Term a)
-  deriving (Functor)
+  deriving (Functor, Show)
 
 data Formula a
-  = Equals (Term a) (Term a) -- The atomic formulae
-  | Top
+  = Top
+  | Equals (Term a) (Term a) -- The atomic formulae
   | And (Formula a) (Formula a)
   | Or (Formula a) (Formula a)
   | Not (Formula a)
   | Forall a (Formula a)
   | Exists a (Formula a)
+  deriving (Show)
 
 preprocess :: Formula String -> Formula Int
-preprocess p =
-  evalState
-    ( prenex <$> (standardise p >>= normalise)
-    )
-    ([], 0)
+preprocess = prenex . normalise . standardise
 
--- Allows us to keep track of variables names, and create fresh ones
-type Scope = State ([(String, Int)], Int)
+-- Create a unique number of variables in a sentance, i.e. a formula with no free variables
+standardise :: Formula String -> Formula Int
+standardise = go []
+  where
+    -- Register a new variable
+    insert :: String -> [(String, Int)] -> (Int, [(String, Int)])
+    insert x [] = (0, [(x, 0)])
+    insert x m =
+      let y = maximum [z | (_, z) <- m] + 1
+       in (y, (x, y) : m)
 
-fresh :: Scope Int
-fresh = do
-  (m, x) <- get
-  put (m, x + 1)
-  return x
-
--- Record all fresh variables that a function produces
-listen :: Scope a -> Scope (a, [Int])
-listen ma = do
-  (_, x) <- get
-  a <- ma
-  (_, x') <- get
-  return (a, [x .. (x' - 1)])
-
--- Record the standardisation of a variable
-addToScope :: String -> Int -> Scope ()
-addToScope x x' = do
-  (m, y) <- get
-  put ((x, x') : m, max y x')
-
--- Rename the variables in a term
-rename :: Term String -> Scope (Term Int)
-rename t = do
-  (m, _) <- get
-  return
-    ( fmap
+    -- Rename the variables in a term
+    rename :: [(String, Int)] -> Term String -> Term Int
+    rename m =
+      fmap
         ( \a ->
             case lookup a m of
               Nothing -> error "Variable not in scope!"
               Just b -> b
         )
-        t
-    )
 
--- Create a unique number of variables in a sentance, i.e. a formula with no free variables
-standardise :: Formula String -> Scope (Formula Int)
-standardise Top = return Top
-standardise (Equals x y) = do
-  x' <- rename x
-  y' <- rename y
-  return (Equals x' y')
-standardise (And p q) = do
-  p' <- standardise p
-  q' <- standardise q
-  return (And p' q')
-standardise (Or p q) = do
-  p' <- standardise p
-  q' <- standardise q
-  return (Or p' q')
-standardise (Not p) = do
-  p' <- standardise p
-  return (Not p')
-standardise (Forall x p) = do
-  x' <- fresh
-  addToScope x x'
-  p' <- standardise p
-  return (Forall x' p')
-standardise (Exists x p) = do
-  x' <- fresh
-  addToScope x x'
-  p' <- standardise p
-  return (Exists x' p')
+    go :: [(String, Int)] -> Formula String -> Formula Int
+    go _ Top = Top
+    go m (Equals x y) = Equals (rename m x) (rename m y)
+    go m (And p q) = And (go m p) (go m q)
+    go m (Or p q) = Or (go m p) (go m q)
+    go m (Not p) = Not (go m p)
+    go m (Forall x p) =
+      let (y, m') = insert x m
+       in Forall y (go m' p)
+    go m (Exists x p) =
+      let (y, m') = insert x m
+       in Exists y (go m' p)
+
+fresh :: State Int Int
+fresh = do
+  x <- get
+  put (x + 1)
+  return x
 
 -- Turn a term into a formula and a variable which has the same value
-termToVar :: Term Int -> Scope (Formula Int, Int)
+termToVar :: Term Int -> State Int (Formula Int, Int)
 termToVar t = do
-  ((p, y), xs) <- listen (go t)
-  return (foldr Exists p xs, y)
+  x <- get
+  (p, y) <- go t
+  x' <- get
+  return (foldr Exists p [x .. (x' - 1)], y)
   where
     go (Var x) = return (Top, x)
     go Zero = do
@@ -113,30 +85,40 @@ termToVar t = do
       return (And p (And q (Equals (Add (Var x) (Var y)) (Var z))), z)
 
 -- Convert all atoms to one of the forms: x + y = z, x = 0, or y = 1
-normalise :: Formula Int -> Scope (Formula Int)
-normalise Top = return Top
-normalise (Equals t1 t2) = do
-  (p, x) <- termToVar t1
-  (q, y) <- termToVar t2
-  z <- fresh
-  return (Exists z (And p (And q (And (Equals (Var x) Zero) (Equals (Add (Var y) (Var z)) (Var z))))))
-normalise (And p q) = do
-  p' <- normalise p
-  q' <- normalise q
-  return (And p' q')
-normalise (Or p q) = do
-  p' <- normalise p
-  q' <- normalise q
-  return (Or p' q')
-normalise (Not p) = fmap Not (normalise p)
-normalise (Forall x p) = fmap (Forall x) (normalise p)
-normalise (Exists x p) = fmap (Exists x) (normalise p)
+normalise :: Formula Int -> Formula Int
+normalise p = evalState (go p) 0
+  where
+    go :: Formula Int -> State Int (Formula Int)
+    go Top = return Top
+    go (Equals t1 t2) = do
+      z <- fresh
+      (p, x) <- termToVar t1
+      (q, y) <- termToVar t2
+      return $ Exists z $ And p $ And q $ And (Equals (Var z) Zero) (Equals (Add (Var x) (Var z)) (Var y))
+    go (And p q) = do
+      p' <- go p
+      q' <- go q
+      return (And p' q')
+    go (Or p q) = do
+      p' <- go p
+      q' <- go q
+      return (Or p' q')
+    go (Not p) = Not <$> go p
+    go (Forall x p) = do
+      y <- get
+      put (max (x + 1) y)
+      Forall x <$> go p
+    go (Exists x p) = do
+      y <- get
+      put (max (x + 1) y)
+      Exists x <$> go p
 
 -- Convert a standardised formula to prenex normal form
 prenex :: Formula a -> Formula a
 prenex Top = Top
 prenex (Equals x y) = Equals x y
 prenex (And p q) =
+  -- TODO: Ordering when both p and q are quantifiers
   case (prenex p, prenex q) of
     (Forall x p', q') -> Forall x (prenex (And p' q'))
     (Exists x p', q') -> Exists x (prenex (And p' q'))
@@ -144,6 +126,7 @@ prenex (And p q) =
     (p', Exists x q') -> Exists x (prenex (And p' q'))
     (p', q') -> And p' q'
 prenex (Or p q) =
+  -- TODO: Ordering when both p and q are quantifiers
   case (prenex p, prenex q) of
     (Forall x p', q') -> Forall x (prenex (Or p' q'))
     (Exists x p', q') -> Exists x (prenex (Or p' q'))
